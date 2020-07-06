4060def9fc3c6c78b0aacbf467569ae5c556734d
cosiwa <collinssimiyu85@gmail.com>
2020-07-06 18:44:46 +0300

    just deleted some scrappy test code

--- learn.php
+++ learn.php
@@ -6,41 +6,10 @@
 </head>
 <body>
 
-    <h2>Multidimensional arrays</h2>
-    <?php
+    <h2>Collins Git repository</h2>
 
-   	
-$host="localhost";
-$username="root";
-$password="";
-$database_name="user";
-
-//the database connection variable is $dbc03
-
-
-
-$dbc=mysqli_connect($host,$username,$password,$database_name);
-
-if ($dbc) {
-  # code...
-  //select all data from the databases
-  $q="SELECT * FROM cosa_user where user_id=4";
-
-  $result=mysqli_query($dbc,$q);
-
-  if ($result) {
-    # code...
-    echo "Results available....<br>";
-
-    while ($row=mysqli_fetch_assoc($result)) {
-      # code...
-      $name=$row['email'];
-      echo "$name";
-    }
-  }
-}
-
-    ?>
+    
+  
     <script type="text/javascript" src="js/Validate.js"></script>
     <script type="text/javascript">
   
