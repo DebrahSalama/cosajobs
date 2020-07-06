2782b2a7d6c36af2ff5fafc93cad93d14b545a5b
DebrahSalama <67915811+DebrahSalama@users.noreply.github.com>
2020-07-06 19:03:11 +0300

    Update welcome.php
    Added a nice description message

--- welcome.php
+++ welcome.php
@@ -18,14 +18,15 @@ include "included_files/header/header.php";
 	<!-- welcome the user -->
 	<section>
 		<blockquote>
-			"Hey user thank you for joining COSA.You are now ready to find and post jobs as well"
+			"Hey user thank you for joining COSA.You are now ready to find and post jobs as well..<br>Our mission is to organize world's
+			jobs and make them universally accessible and useful"
 		</blockquote>
 	</section>
 
 	<!--Redirect the user to site if he/she clicks button-->
 
 	<div id="siteRedirectionLinks">
-
+		
 		<a href="http://localhost:8080/app/home.php" id="homePage">Continue to site</a>
 		<a href="http://localhost:8080/app/about_us.php" id="aboutUs">Find more about us</a>
 
